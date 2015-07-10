99ae08ee166caa9d399dc3f1a16b330ede85c7c9
heiheychan <Bill_Chan@Chaks-MacBook-Air.local>
2015-07-11 02:06:41 +0800

    First Commit: finished html + css and built the getNameGetStart jquery function

---
+++ js/application.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+var playerOneName;
+var playerTwoName;
+
+var getNameGetStart = function (){
+	if ($('.p1Name').val().length != 0 && $('.p2Name').val().length != 0) {
+		playerOneName = $('.p1Name').val();
+		playerTwoName = $('.p2Name').val();
+	} else {
+	}
+}
+
+$(document).on('click', '.getStarted', function(){
+
+
+	
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
